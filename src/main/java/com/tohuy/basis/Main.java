fe7ce0f96b8c1e7f545bcaf47137373934c33306
ToHuy1802 <totanhuypro@gmail.com>
2026-05-14 09:03:01 +0700

    demo git

---
+++ src/main/java/com/tohuy/basis/Main.java
@@ -0,0 +1,10 @@
+package com.tohuy.basis;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        System.out.println("Hello Mavan and JDK-24");
+        System.out.println("Hello Git and JDK-24");
+    }
+}
